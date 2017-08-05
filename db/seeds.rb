1997c900d94f8a6916e4d4078c3c9ef63eb26253
BLESSING ALFRED <blealf@gmail.com>
2017-08-05 14:36:47 +0100

    Push all

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,2 @@
 
-Accesory.create!([
-  {title: "Contact Here!", body: "<p><em><strong>Coming Soon!!!</strong></em></p>\r\n", acc_type: "Contact"},
-  {title: "Terms and Conditions Here!", body: "<p><em><strong>Coming Soon!!!</strong></em></p>\r\n", acc_type: "Term"},
-  {title: "About Here!", body: "<p><em><strong>Coming Soon!!!</strong></em></p>\r\n", acc_type: "About"}
-])
 
-User.create!([
-  {firstname: "Blessing", lastname: "Alfred", othernames: "", occupation: "", username: "", email: "admin@yahoo.com", password_digest: "$2a$10$0UcqEHRl/2thQ/uTO9Xa6evUK56YgPTaOMWJhNLXCEQgZNxdoa8de", unit: "CAD", remember_digest: nil, role: "admin"},
-])
